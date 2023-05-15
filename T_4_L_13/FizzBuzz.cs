77660798468bb3dac8251e46444e276653c19197
AndrzejZaba <andrzejzaba@onet.pl>
2023-05-15 18:24:13 +0200

    Add project files.

---
+++ T_4_L_13/FizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_4_L_13
+{
+    public class FizzBuzz
+    {
+        public string Play(int number)
+        {
+            if(number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+            if(number % 3 == 0 ) return "Fizz";
+            if(number % 5 == 0) return "Buzz";
+            return number.ToString();            
+        }
+    }
+}
---
+++ T_4_L_13/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace T_4_L_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new FizzBuzz();
+            Console.WriteLine("Zagrajmy w FizzBuzz!\n");
+
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę:");
+                if(int.TryParse(Console.ReadLine(), out int value))
+                {
+                    Console.WriteLine($"Wynik:\t{game.Play(value)}\n");
+                }
+                else
+                {
+                    Console.WriteLine("Podano złą wartość\n");
+                }
+            }
+        }
+    }
+}
